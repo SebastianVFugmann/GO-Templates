6df7dc474b0003dcbd1ec58208c7f912b37f95d5
Sebastian Fugmann <sefu@itu.dk>
2021-12-27 12:15:56 +0100

    Done

--- Client-server/Client/Client.go
+++ Client-server/Client/Client.go
@@ -3,11 +3,8 @@ package main
 import (
 	"bufio"
 	"context"
-	"fmt"
-	"io"
 	"log"
 	"os"
-	"time"
 
 	pb "github.com/SebastianVFugmann/GO-Templates/Client-server/Proto"
 	"github.com/SebastianVFugmann/GO-Templates/LamportClock"
@@ -55,7 +52,7 @@ func main() {
 		conn.Close()
 	}, 0)
 
-	createID(ctx, c)
+	createID(c, ctx)
 
 	//Gets the client to join the system and listens for messages, hence it is a go routine
 	go join(ctx, c)
@@ -63,17 +60,17 @@ func main() {
 	//Waits for the user to enter messages and sends them
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-		go method(ctx, c, scanner.Text(), false)
+		go method(ctx, c, scanner.Text())
 	}
 }
 
 //Private method that calls the SendMessage method defined in the proto file
-func method(ctx context.Context, client pb.ServiceClient, message string, special bool) {
+func method(ctx context.Context, client pb.ServiceClient, message string) {
 	//Increment clock since local event happened
 	myClock.Increment()
 
 	//Gets the stream returned by the SendMessage method
-	stream, err := client.Method(ctx)
+	/*stream, err := client.Method(ctx)
 	if err != nil {
 		log.Printf("Cannot send message: error: %v \n", err)
 	}
@@ -89,58 +86,59 @@ func method(ctx context.Context, client pb.ServiceClient, message string, specia
 		fmt.Printf("T:%d - %v returned when sending message - %v\n", ack.Time, ack.HttpStatusCode, err2)
 	}
 	myClock.SyncTime(ack.Time)
-	log.Printf("T:%d - Message sent: %v \n", ack.Time, ack.HttpStatusCode)
+	log.Printf("T:%d - Message sent: %v \n", ack.Time, ack.HttpStatusCode)*/
 }
 
 //A private method created to call the Join method defined in the proto file
-func join(ctx context.Context, client pb.ChittyChatServiceClient) {
+func join(ctx context.Context, client pb.ServiceClient) {
 	//Increase clock, since new event happened
 	myClock.Increment()
 
-	//Creates the clients join request
-	join := &pb.JoinRequest{
-		Id:   MyId,
-		Time: myClock.GetTime(),
-	}
-
-	//Gets the stream that is returned by the JoinChannel method defined in the proto
-	stream, err := client.Join(ctx, join)
-	if err != nil {
-		log.Fatalf("client.JoinChannel(ctx, &channel) throws: %v", err)
-	}
-	//Sleeps to allow the new user to have it's channel created
-	fmt.Printf("Connecting to the Chitty Chat...\n")
-	time.Sleep(2 * time.Second)
-
-	//Creates an infinite loop that makes sure the stream is always open and waiting for messages
-	waitc := make(chan struct{})
-
-	go func() {
-		for {
-			in, err := stream.Recv()
-			if err == io.EOF {
-				close(waitc)
-				return
-			}
-			if err != nil {
-				log.Fatalf("Failed to receive message from channel joining. \nErr: %v", err)
-			}
-
-			if in.Special {
-				//Syncs time with the received message
-				myClock.SyncTime(in.Time)
-				//Prints message in specific format
-				fmt.Printf("T:%d - %v %v \n", myClock.GetTime(), in.Id, in.Message)
-			} else if MyId != in.Id {
-				//Syncs time with the received message
-				myClock.SyncTime(in.Time)
-				//Prints message
-				fmt.Printf("T:%d - %v -> %v \n", myClock.GetTime(), in.Id, in.Message)
-			}
+	/*
+		//Creates the clients join request
+		join := &pb.JoinRequest{
+			Id:   MyId,
+			Time: myClock.GetTime(),
 		}
-	}()
 
-	<-waitc
+		//Gets the stream that is returned by the JoinChannel method defined in the proto
+		stream, err := client.Join(ctx, join)
+		if err != nil {
+			log.Fatalf("client.JoinChannel(ctx, &channel) throws: %v", err)
+		}
+		//Sleeps to allow the new user to have it's channel created
+		fmt.Printf("Connecting to the Chitty Chat...\n")
+		time.Sleep(2 * time.Second)
+
+		//Creates an infinite loop that makes sure the stream is always open and waiting for messages
+		waitc := make(chan struct{})
+
+		go func() {
+			for {
+				in, err := stream.Recv()
+				if err == io.EOF {
+					close(waitc)
+					return
+				}
+				if err != nil {
+					log.Fatalf("Failed to receive message from channel joining. \nErr: %v", err)
+				}
+
+				if in.Special {
+					//Syncs time with the received message
+					myClock.SyncTime(in.Time)
+					//Prints message in specific format
+					fmt.Printf("T:%d - %v %v \n", myClock.GetTime(), in.Id, in.Message)
+				} else if MyId != in.Id {
+					//Syncs time with the received message
+					myClock.SyncTime(in.Time)
+					//Prints message
+					fmt.Printf("T:%d - %v -> %v \n", myClock.GetTime(), in.Id, in.Message)
+				}
+			}
+		}()
+
+		<-waitc*/
 }
 
 func leave(ctx context.Context, client pb.ServiceClient) {
